9d4e0b6ab437c17d7bb7fd800674e1caac9377e1
Jules Legueux <jules.legueux@carminecapital.com>
2025-08-15 20:41:12 +0200

    feat: create base faq modal component / events

--- src/app/features/faq/faq.component.ts
+++ src/app/features/faq/faq.component.ts
@@ -1,14 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  IonButton,
+  IonIcon,
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonContent,
+} from '@ionic/angular/standalone';
+import { informationCircle } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss'],
+  imports: [
+    IonButton,
+    IonIcon,
+    IonModal,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonContent,
+  ],
 })
-export class FaqComponent  implements OnInit {
+export class FaqComponent implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
 
-  constructor() { }
+  constructor() {
+    addIcons({ informationCircle });
+  }
 
   ngOnInit() {}
 
+  onWillDismiss(event: any) {
+    console.log('onWillDismiss', event);
+  }
+
+  cancel() {
+    this.modal.dismiss();
+  }
+
+  confirm() {
+    this.modal.dismiss();
+  }
 }
