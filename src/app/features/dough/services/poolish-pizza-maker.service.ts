53e3008d0b13b14cc03c58ea8728b0626b4e771a
Jules Legueux <jules.legueux@carminecapital.com>
2025-08-15 14:08:48 +0200

    update yeast service import

--- src/app/features/dough/services/poolish-pizza-maker.service.ts
+++ src/app/features/dough/services/poolish-pizza-maker.service.ts
@@ -5,7 +5,7 @@ import {
   POOLISH_RATIO_BY_TEMPERATURE,
 } from '../constants';
 import { PoolishPizzaFormData } from '../dough-form/dough-form.component';
-import { YeastService } from 'src/app/shared/services/yeast.service';
+import { YeastService } from 'src/app/features/dough/services/yeast.service';
 import { PIZZA_WEIGHT, SALT_WEIGHT_PER_PIZZA } from 'src/app/shared/constants';
 
 export interface PoolishPizzaResult {
