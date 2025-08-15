93d8200fdbb506e877ad2c65c8debebd96f15b5c
Jules Legueux <jules.legueux@carminecapital.com>
2025-08-16 00:55:18 +0200

    update translations / fix number formatting

--- src/app/features/dough/dough-quantity/dough-quantity.component.ts
+++ src/app/features/dough/dough-quantity/dough-quantity.component.ts
@@ -2,13 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { DoughResult } from '../services/dough-calculator.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationKeys } from '../../../shared/services/translation-keys.service';
-import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dough-quantity',
   templateUrl: './dough-quantity.component.html',
   styleUrls: ['./dough-quantity.component.scss'],
-  imports: [TranslateModule, DecimalPipe],
+  imports: [TranslateModule],
   standalone: true,
 })
 export class DoughQuantityComponent implements OnInit {
@@ -19,4 +18,8 @@ export class DoughQuantityComponent implements OnInit {
 
   constructor() {}
   ngOnInit() {}
+
+  round(value: number) {
+    return Math.round(value);
+  }
 }
