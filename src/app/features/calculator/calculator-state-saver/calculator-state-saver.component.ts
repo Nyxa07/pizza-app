203fb45420e9496215fb26aeb397a4f773851e9c
Jules Legueux <jules.legueux@pm.me>
2025-09-05 11:41:50 +0200

    add action sheet to save state manager update translations files

--- src/app/features/calculator/calculator-state-saver/calculator-state-saver.component.ts
+++ src/app/features/calculator/calculator-state-saver/calculator-state-saver.component.ts
@@ -1,16 +1,8 @@
-import {
-  Component,
-  computed,
-  Input,
-  OnInit,
-  signal,
-  ViewChild,
-} from '@angular/core';
+import { Component, OnInit, signal, ViewChild } from '@angular/core';
 import {
   FormBuilder,
   FormsModule,
   ReactiveFormsModule,
-  Validators,
   ValidatorFn,
 } from '@angular/forms';
 import {
@@ -26,15 +18,13 @@ import {
   IonListHeader,
   IonLabel,
   IonToast,
-  IonItemSliding,
-  IonItemOption,
-  IonItemOptions,
+  ActionSheetController,
 } from '@ionic/angular/standalone';
-import { TranslatePipe } from '@ngx-translate/core';
-import { LucideAngularModule, SaveIcon } from 'lucide-angular';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { LucideAngularModule, SaveIcon, EllipsisIcon } from 'lucide-angular';
 import { CalculatorInput } from '../services/calculator-state.service';
 import { CalculatorStateSaveManagerService } from '../services/calculator-state-save-manager.service';
-import { JsonPipe } from '@angular/common';
+import { Dialog } from '@capacitor/dialog';
 
 @Component({
   selector: 'app-calculator-state-saver',
@@ -59,9 +49,6 @@ import { JsonPipe } from '@angular/common';
     IonListHeader,
     IonLabel,
     IonToast,
-    IonItemSliding,
-    IonItemOption,
-    IonItemOptions,
     ReactiveFormsModule,
   ],
 })
@@ -71,6 +58,7 @@ export class CalculatorStateSaverComponent implements OnInit {
   toastMessage = signal('');
 
   readonly SaveIcon = SaveIcon;
+  readonly EllipsisIcon = EllipsisIcon;
   protected savedStates = signal<{ name: string; input: CalculatorInput }[]>(
     [],
   );
@@ -89,13 +77,11 @@ export class CalculatorStateSaverComponent implements OnInit {
     stateName: ['', [this.validation]],
   });
 
-  protected stateName = computed<string>(
-    () => this.form.controls.stateName.value ?? '',
-  );
-
   constructor(
     private stateSaveManagerService: CalculatorStateSaveManagerService,
     private fb: FormBuilder,
+    private translate: TranslateService,
+    private actionSheetCtrl: ActionSheetController,
   ) {}
 
   ngOnInit() {
@@ -107,34 +93,97 @@ export class CalculatorStateSaverComponent implements OnInit {
   }
 
   saveNewState() {
-    if (!this.stateName()) {
+    if (!this.form.valid) {
       return;
     }
-    this.stateSaveManagerService.saveState(this.stateName());
+    this.stateSaveManagerService.saveState(this.form.controls.stateName.value!);
     this.savedStates.set(this.stateSaveManagerService.listSavedStates());
-    this.toastMessage.set('calculator.stateSaver.form.stateSaved');
+    this.toastMessage.set('calculator.stateSaver.stateSaved');
     this.toast.present();
     this.form.reset();
   }
 
   overrideState(state: { name: string; input: CalculatorInput }) {
-    this.stateSaveManagerService.saveState(state.name);
+    this.stateSaveManagerService.saveState(this.form.controls.stateName.value!);
     this.savedStates.set(this.stateSaveManagerService.listSavedStates());
-    this.toastMessage.set('calculator.stateSaver.form.stateOverridden');
+    this.toastMessage.set('calculator.stateSaver.stateOverridden');
     this.toast.present();
   }
 
   loadState(state: { name: string; input: CalculatorInput }) {
     this.stateSaveManagerService.loadState(state.name);
-    this.toastMessage.set('calculator.stateSaver.form.stateLoaded');
+    this.toastMessage.set('calculator.stateSaver.stateLoaded');
     this.toast.present();
   }
 
   deleteState(state: { name: string; input: CalculatorInput }) {
     this.stateSaveManagerService.deleteState(state.name);
     this.savedStates.set(this.stateSaveManagerService.listSavedStates());
-    this.toastMessage.set('calculator.stateSaver.form.stateDeleted');
+    this.toastMessage.set('calculator.stateSaver.stateDeleted');
     this.toast.present();
-    this.form.patchValue({ stateName: this.stateName() });
+    this.form.patchValue({ stateName: this.form.controls.stateName.value! });
+  }
+
+  async presentActionSheet(state: { name: string; input: CalculatorInput }) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: this.translate.instant('common.actions.title'),
+      buttons: [
+        {
+          text: this.translate.instant('common.actions.load'),
+          data: {
+            action: 'load',
+          },
+          handler: () => {
+            this.loadState(state);
+            this.modal.dismiss();
+          },
+        },
+        {
+          text: this.translate.instant('common.actions.override'),
+          data: {
+            action: 'override',
+          },
+          handler: async () => {
+            const { value } = await Dialog.confirm({
+              title: 'Confirm',
+              message: this.translate.instant(
+                'calculator.stateSaver.confirmOverride',
+              ),
+            });
+            if (value) {
+              this.overrideState(state);
+              this.modal.dismiss();
+            }
+          },
+        },
+        {
+          text: this.translate.instant('common.actions.delete'),
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+          handler: async () => {
+            const { value } = await Dialog.confirm({
+              title: 'Confirm',
+              message: this.translate.instant(
+                'calculator.stateSaver.confirmDeletion',
+              ),
+            });
+            if (value) {
+              this.deleteState(state);
+            }
+          },
+        },
+        {
+          text: this.translate.instant('common.actions.cancel'),
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
   }
 }
